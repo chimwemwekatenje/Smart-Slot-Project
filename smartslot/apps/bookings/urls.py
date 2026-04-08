efbe8ebcc21fd39a32215645f73de63c5fef71eb
chimwemwekatenje <bsc-26-21@unima.ac.mw>
2026-04-07 20:55:15 -0700

    feat: update bookings urls and minor fixes

--- smartslot/apps/bookings/urls.py
+++ smartslot/apps/bookings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookingListView
+from .views import BookingListView, BookingCreateView
 
 urlpatterns = [
     path('', BookingListView.as_view(), name='booking_list'),
+    path('create/<int:resource_pk>/', BookingCreateView.as_view(), name='booking_create'),
 ]
