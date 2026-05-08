6523779d3f0a83f4c906325dff015c2f26aa57a8
Luekie <bsc-com-03-22@unima.ac.mw>
2026-05-08 03:47:15 +0200

    recent

--- smartslot/config/settings/production.py
+++ smartslot/config/settings/production.py
@@ -1,1 +1,18 @@
 from .base import *
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'smartslot-bh9c.onrender.com,localhost,127.0.0.1').split(',')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://smartslot-bh9c.onrender.com',
+]
+
+# Static files served by WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Security
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
